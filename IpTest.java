c7b4c634f2f1bc0d2550b76eaf97a84e4624800b
artemSSSS <37380834+artemSSSS@users.noreply.github.com>
2021-01-25 21:23:15 +0400

    Add files via upload
    speeded up program

--- IpTest.java
+++ IpTest.java
@@ -29,38 +29,50 @@ public class IpTest {
         long result = 0;
 
         boolean[] myArray = new boolean[1000000000];
+        boolean[] myArray2 = new boolean[1000000000];
+        boolean[] myArray3 = new boolean[1000000000];
+        boolean[] myArray4 = new boolean[1000000000];
+        boolean[] myArray5 = new boolean[1000000000];
 
-        for (int i = 0; i < 5; i++){
-            FileInputStream inputStream = new FileInputStream(originalInputFile);
-            Scanner sc = new Scanner(inputStream, "UTF-8");
+	    FileInputStream inputStream = new FileInputStream(originalInputFile);
+	    Scanner sc = new Scanner(inputStream, "UTF-8");
 
+	    while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			long ip = ipToLong(line);
+			if (ip >= 0 && ip < 1000000000) {
+				myArray[(int)(ip)] = true;
+			} else if (ip >= 1000000000 && ip < 2000000000) {
+				myArray2[(int)(ip - 1000000000)] = true;
+			} else if (ip >= 2000000000 && ip < 3000000000L) {
+				myArray3[(int)(ip - 2000000000L)] = true;
+			} else if (ip >= 3000000000L && ip < 4000000000L) {
+				myArray4[(int)(ip - 3000000000L)] = true;
+			} else if (ip >= 4000000000L && ip < 5000000000L) {
+				myArray5[(int)(ip - 4000000000L)] = true;
+			}
+	    }
 
-            while (sc.hasNextLine()) {
-                String line = sc.nextLine();
-                long ip = ipToLong(line);
-                if (i == 0 && ip >= 0 && ip < 1000000000) {
-                    myArray[(int)(ip)] = true;
-                } else if ( i == 1 && ip >= 1000000000 && ip < 2000000000) {
-                    myArray[(int)(ip - 1000000000)] = true;
-                } else if ( i == 2 && ip >= 2000000000 && ip < 3000000000L) {
-                    myArray[(int)(ip - 2000000000L)] = true;
-                } else if ( i == 3 && ip >= 3000000000L && ip < 4000000000L) {
-                    myArray[(int)(ip - 3000000000L)] = true;
-                } else if ( i == 4 && ip >= 4000000000L && ip < 5000000000L) {
-                    myArray[(int)(ip - 4000000000L)] = true;
-                }
-            }
+	    for (int j = 0; j < 1000000000; j++) {
+			if (myArray[j]) {
+				result += 1;
+			}
+			if (myArray2[j]) {
+				result += 1;
+			}
+			if (myArray3[j]) {
+				result += 1;
+			}
+			if (myArray4[j]) {
+				result += 1;
+			}
+			if (myArray5[j]) {
+				result += 1;
+			}
+	    }
 
-            for (int j = 0; j < 1000000000; j++) {
-                if (myArray[j]) {
-                    result += 1;
-                }
-                myArray[j] = false;
-            }
-
-            inputStream.close();
-            sc.close();
-        }
+	    inputStream.close();
+	    sc.close();
 
         System.out.println("There is " + String.valueOf(result) + " unique ip addresses in file " + originalInputFile);
     }
